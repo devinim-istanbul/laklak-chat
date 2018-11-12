798566464e0e48aa596b9dda4971babc17100883
Berkan Yavri <berkan.yavri@deliveryhero.com>
2018-11-13 00:09:42 +0100

    WIP

---
+++ laklak-command-builder/src/lib.rs
@@ -0,0 +1,79 @@
+extern crate base64;
+
+use base64::{encode, decode};
+use std::marker::PhantomData;
+
+#[derive(Debug)]
+pub struct Command<T> {
+    pub _marker: PhantomData<T>,
+}
+
+#[derive(Debug)]
+pub struct SendMessage {
+    recipient: String,
+    message: String,
+}
+
+#[derive(Debug)]
+pub struct Authenticate {
+    token: String,
+}
+
+#[derive(Debug)]
+pub struct Ping {}
+
+#[derive(Debug)]
+pub struct Pong {}
+
+macro_rules! id_like {
+    ($token:expr) => {
+        if $token.len() != 16 {
+            panic!("Invalid token: {}", $token);
+        } else {
+            $token
+        };
+    }
+}
+
+impl Command<SendMessage> {
+    pub fn build(recipient: String, message: String) -> SendMessage {
+        SendMessage { recipient: recipient, message: message }
+    }
+}
+
+impl Command<Authenticate> {
+    pub fn build(token: String) -> Authenticate {
+        Authenticate { token: id_like!(token) }
+    }
+}
+
+impl Command<Ping> {
+    pub fn build() -> Ping {
+        Ping {}
+    }
+}
+
+impl Command<Pong> {
+    pub fn build() -> Pong {
+        Pong {}
+    }
+}
+
+pub fn encode_base46(s: &str) -> String {
+    encode(s)
+}
+
+pub fn decode_base64<'a>(s: &'a str) -> String {
+    decode(s)
+        .map_err(|e| ParserFailure::Base64DecodeError(e))
+        .and_then(|buf|
+            String::from_utf8(buf).map_err(|e| ParserFailure::Utf8EncodingError(e))
+        )
+        .unwrap()
+}
+
+#[derive(Debug)]
+pub enum ParserFailure {
+    Base64DecodeError(base64::DecodeError),
+    Utf8EncodingError(std::string::FromUtf8Error)
+}
---
+++ laklak-command-builder/tests/test_type.rs
@@ -0,0 +1,62 @@
+extern crate laklak_command_builder;
+
+use laklak_command_builder::*;
+
+// #[test]
+// fn decode_send_message_command() {
+//   let message = "U0VORHwxMjM0NTY3ODEyMzQ1Njc4fEhlbGxvLCBXb3JsZCE=";
+//   let command = Decode!(message);
+//   assert_eq!(
+//     command,
+//     Command::<SendMessage>::build(String::from("1234567812345678"), String::from("Hello, World!"))
+//   );
+// }
+
+// #[test]
+// fn decode_auth_command() {
+//   let message = "QVVUSHwxMjM0NTY3ODEyMzQ1Njc4";
+//   let command = Decode!(message);
+//   assert_eq!(
+//     command,
+//     Command::<Authenticate>::build(String::from("1234567812345678"))
+//   );
+// }
+
+// #[test]
+// fn decode_ping_command() {
+//   let message = "UElORw==";
+//   let command = Decode!(message);
+//   assert_eq!(command, Command::<Ping>::build());
+// }
+
+// #[test]
+// fn decode_pong_command() {
+//   let message = "UE9ORw=="; // Somehow, this prints ping as well /shrug
+//   let command = Decode!(message);
+//   println!("{:?}", command);
+//   assert_eq!(command, Command::<Pong>::build());
+// }
+
+// #[test]
+// #[should_panic]
+// fn panic_on_bad_token() {
+//   let message = "AUTH|12345678";
+//   Decode!(message);
+// }
+
+// #[test]
+// #[should_panic]
+// fn panic_on_unknown_command() {
+//   let message = "GREETINGS";
+//   Decode!(message);
+// }
+
+// #[test]
+// fn encode_command() {
+//   let send_message = Command::<SendMessage>::build(
+//     String::from("1234567812345678"),
+//     String::from("Hello, World!")
+//   );
+//   let command_string = Encode!{send_message};
+//   assert_eq!(command_string, "U0VORHwxMjM0NTY3ODEyMzQ1Njc4fEhlbGxvLCBXb3JsZCE=");
+// }
